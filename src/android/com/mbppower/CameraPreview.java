d81a40d151afaa788a82c20a400821d793ff3ff7
Sergey Kanygin <skanygin@ronasit.com>
2015-09-17 15:56:30 +0600

    Switch camera on start

--- src/android/com/mbppower/CameraPreview.java
+++ src/android/com/mbppower/CameraPreview.java
@@ -84,7 +84,7 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
                     int width = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, args.getInt(2), metrics);
                     int height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, args.getInt(3), metrics);
 
-                    String defaultCamera = args.getString(4);
+                    String defaultCamera = invertCamera(args.getString(4));
                     Boolean tapToTakePicture = args.getBoolean(5);
                     Boolean dragEnabled = args.getBoolean(6);
                     Boolean toBack = args.getBoolean(7);
@@ -127,7 +127,13 @@ public class CameraPreview extends CordovaPlugin implements CameraActivity.Camer
                 }
             }
         });
+
+        fragment.switchCamera();
         return true;
+    }
+
+    private String invertCamera(String originalCamera){
+        return originalCamera == "front" ? "back" : "front";
     }
 
     private boolean takePicture(final JSONArray args, CallbackContext callbackContext) {
