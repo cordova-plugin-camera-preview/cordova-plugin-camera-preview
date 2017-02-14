9be3c15560e8d7592d5c9dc4c3778c9d8b343ae9
Weston Ganger <westonganger@gmail.com>
2017-02-14 08:06:50 -0800

    fix android merge bugs

--- src/android/CustomSurfaceView.java
+++ src/android/CustomSurfaceView.java
@@ -5,21 +5,21 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
-    private final String TAG = "CustomSurfaceView";
+  private final String TAG = "CustomSurfaceView";
 
-    CustomSurfaceView(Context context){
-        super(context);
-    }
+  CustomSurfaceView(Context context){
+    super(context);
+  }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-    }
+  @Override
+  public void surfaceCreated(SurfaceHolder holder) {
+  }
 
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-    }
+  @Override
+  public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+  }
 
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-    }
+  @Override
+  public void surfaceDestroyed(SurfaceHolder holder) {
+  }
 }
--- src/android/TapGestureDetector.java
+++ src/android/TapGestureDetector.java
@@ -4,19 +4,20 @@ import android.view.GestureDetector;
 import android.view.MotionEvent;
 
 class TapGestureDetector extends GestureDetector.SimpleOnGestureListener{
+  private final String TAG = "TapGestureDetector";
 
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
+  @Override
+  public boolean onDown(MotionEvent e) {
+    return false;
+  }
 
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return true;
-    }
+  @Override
+  public boolean onSingleTapUp(MotionEvent e) {
+    return true;
+  }
 
-    @Override
-    public boolean onSingleTapConfirmed(MotionEvent e) {
-        return true;
-    }
+  @Override
+  public boolean onSingleTapConfirmed(MotionEvent e) {
+    return true;
+  }
 }
