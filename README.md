4e2c90816e2b1dea34be0b4fca4077345e3ead7d
Weston Ganger <westonganger@gmail.com>
2017-03-05 09:09:20 -0800

    updated with latest api and features

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-cordova-plugin-camera-preview Sample Cordova App
+cordova-plugin-camera-previewSample Cordova App
 =======================
 
-##Install
+This is a sample cordova application for <a href="https://github.com/cordova-plugin-camera-preview/cordova-plugin-camera-preview">cordova-plugin-camera-preview</a>
+
+## Install
 ```
 cordova platform add android
 cordova build android
@@ -10,7 +12,21 @@ cordova platform add ios
 cordova build ios
 ```
 
+## Update
 
-IOS Quirks: It is not possible to use your computers webcam during testing in the simulator, you must device test
+If you last installed an older version of the plugin and want to ensure the sample app is up to date again just do the following to reset.
+
+```
+rm -rf platforms/ plugins/
+
+cordova platform add android
+cordova build android
+
+cordova platform add ios
+cordova build ios
+```
+
+
+**IOS Quirks: It is not possible to use your computers webcam during testing in the simulator, you must device test**
 
 <a href="https://github.com/cordova-plugin-camera-preview/cordova-plugin-camera-preview">cordova-plugin-camera-preview</a> sample cordova app.
--- www/js/index.js
+++ www/js/index.js
@@ -12,7 +12,7 @@ var app = {
   },
 
   takePicture: function(){
-    CameraPreview.takePicture({maxWidth: window.device.width, maxHeight: window.device.height});
+    CameraPreview.takePicture({width: window.device.width, height: window.device.height, quality: 90});
   },
 
   switchCamera: function(){
@@ -27,27 +27,76 @@ var app = {
     CameraPreview.hide();
   },
 
-  colorEffectChanged: function(){
-    var effect = document.getElementById('colorEffectCombo').value;
+  changeColorEffect: function(){
+    var effect = document.getElementById('selectColorEffect').value;
     CameraPreview.setColorEffect(effect);
   },
+
+  changeFlashMode: function(){
+    var mode = document.getElementById('selectFlashMode').value;
+    CameraPreview.setFlashMode(mode);
+  },
+
+  changeZoom: function(){
+    var zoom = document.getElementById('zoomSlider').value;
+    document.getElementById('zoomValue').text = zoom;
+    CameraPreview.setZoom(zoom);
+  },
+
+  changePreviewSize: function(){
+    window.smallPreview = !window.smallPreview;
+    if(window.smallPreview){
+      CameraPreview.setPreviewSize({width: 100, height: 100});
+    }else{
+      CameraPreview.setPreviewSize({width: window.device.width, height: window.device.height});
+    }
+  },
+
+  showSupportedSize: function(type){
+    if(type === 'picture'){
+      CameraPreview.getSupportedPictureSize(function(dimensions){
+        alert("Supported Picture Size\n\nWidth: " + dimensions.width + "\nHeight: " + 'dimensions.height');
+      });
+    }else{
+      CameraPreview.getSupportedPreviewSize(function(dimensions){
+        alert("Supported Preview Size\n\nWidth: " + dimensions.width + "\nHeight: " + 'dimensions.height');
+      });
+    }
+  }
+
 
   init: function(){
     document.getElementById('startCameraButton').addEventListener('click', this.startCamera, false);
     document.getElementById('startCameraAnotherPosButton').addEventListener('click', this.startCameraAnotherPos, false);
 
     document.getElementById('stopCameraButton').addEventListener('click', this.stopCamera, false);
-    document.getElementById('takePictureButton').addEventListener('click', this.takePicture, false);
     document.getElementById('switchCameraButton').addEventListener('click', this.switchCamera, false);
     document.getElementById('showButton').addEventListener('click', this.show, false);
     document.getElementById('hideButton').addEventListener('click', this.hide, false);
-    document.getElementById('colorEffectCombo').addEventListener('change', this.colorEffectChanged, false);
-    //window.addEventListener('orientationchange', this.onStopCamera, false);
     
-    CameraPreview.setOnPictureTakenHandler(function(result){
-      document.getElementById('originalPicture').src = result[0]; //originalPicturePath;
-      document.getElementById('previewPicture').src = result[1]; //previewPicturePath;
+    CameraPreview.setOnPictureTakenHandler(function(imgData){
+      document.getElementById('originalPicture').src = 'data:image/jpeg;base64,' + imgData;
     });
+
+    document.getElementById('takePictureButton').addEventListener('click', this.takePicture, false);
+
+    document.getElementById('selectColorEffect').addEventListener('change', this.changeColorEffect, false);
+
+    if(device.platform === 'Android'){
+      document.getElementById('selectFlashMode').addEventListener('change', this.changeFlashMode, false);
+      document.getElementById('zoomSlider').addEventListener('change', this.changeZoom, false);
+    }else{
+      document.getElementById('androidOnly').style.display = 'none';
+    }
+
+    window.smallPreview = false;
+    document.getElementById('togglePreviewSize').addEventListener('click', this.changePreviewSize, false);
+
+    document.getElementById('showSupportedPreviewSize').addEventListener('click', this.showSupportedSize('preview'), false);
+    document.getElementById('showSupportedPictureSize').addEventListener('click', this.showSupportedSize('picture'), false);
+
+    // legacy - not sure if this was supposed to fix anything
+    //window.addEventListener('orientationchange', this.onStopCamera, false);
   }
 };
 
