d6eba662a4aab0e58d375b261689aef9d9f93ccd
Weston Ganger <westonganger@gmail.com>
2017-03-05 19:33:26 -0800

    fixes

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,9 @@
 var app = {
-  startCamera: function(){
+  startCameraAbove: function(){
     CameraPreview.startCamera({x: 50, y: 50, width: 300, height: 300, toBack: false, previewDrag: true, tapPhoto: true});
   },
 
-  startCameraAnotherPos: function(){
+  startCameraBelow: function(){
     CameraPreview.startCamera({x: 50, y: 50, width: 300, height:300, camera: "front", tapPhoto: true, previewDrag: false, toBack: true});
   },
 
@@ -12,7 +12,7 @@ var app = {
   },
 
   takePicture: function(){
-    CameraPreview.takePicture({width: window.device.width, height: window.device.height, quality: 90});
+    CameraPreview.takePicture({width: window.screen.width, height: window.screen.height, quality: 90});
   },
 
   switchCamera: function(){
@@ -39,7 +39,7 @@ var app = {
 
   changeZoom: function(){
     var zoom = document.getElementById('zoomSlider').value;
-    document.getElementById('zoomValue').text = zoom;
+    document.getElementById('zoomValue').textContent = zoom;
     CameraPreview.setZoom(zoom);
   },
 
@@ -48,25 +48,33 @@ var app = {
     if(window.smallPreview){
       CameraPreview.setPreviewSize({width: 100, height: 100});
     }else{
-      CameraPreview.setPreviewSize({width: window.device.width, height: window.device.height});
+      CameraPreview.setPreviewSize({width: window.screen.width, height: window.screen.height});
     }
   },
 
   showSupportedPreviewSize: function(){
     CameraPreview.getSupportedPreviewSize(function(dimensions){
-      alert("Supported Preview Size\n\nWidth: " + dimensions.width + "\nHeight: " + 'dimensions.height');
+      if(typeof dimensions === 'string'){
+        console.log(dimensions);
+      }else{
+        dimensions.forEach(function(dimension){
+          console.log(dimension.width + 'x' + dimension.height);
+        });
+      }
     });
   },
 
   showSupportedPictureSize: function(){
     CameraPreview.getSupportedPictureSize(function(dimensions){
-      alert("Supported Picture Size\n\nWidth: " + dimensions.width + "\nHeight: " + 'dimensions.height');
+      dimensions.forEach(function(dimension) {
+        console.log(dimension.width + 'x' + dimension.height);
+      });
     });
   },
 
   init: function(){
-    document.getElementById('startCameraButton').addEventListener('click', this.startCamera, false);
-    document.getElementById('startCameraAnotherPosButton').addEventListener('click', this.startCameraAnotherPos, false);
+    document.getElementById('startCameraAboveButton').addEventListener('click', this.startCameraAbove, false);
+    document.getElementById('startCameraBelowButton').addEventListener('click', this.startCameraBelow, false);
 
     document.getElementById('stopCameraButton').addEventListener('click', this.stopCamera, false);
     document.getElementById('switchCameraButton').addEventListener('click', this.switchCamera, false);
@@ -90,7 +98,7 @@ var app = {
     }
 
     window.smallPreview = false;
-    document.getElementById('chagePreviewSize').addEventListener('click', this.changePreviewSize, false);
+    document.getElementById('changePreviewSize').addEventListener('click', this.changePreviewSize, false);
 
     document.getElementById('showSupportedPreviewSize').addEventListener('click', this.showSupportedPreviewSize, false);
     document.getElementById('showSupportedPictureSize').addEventListener('click', this.showSupportedPictureSize, false);
