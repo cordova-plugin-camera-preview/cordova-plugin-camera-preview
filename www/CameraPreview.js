c10a4683c992ae8b0047340d1ff3f6f5d4e66ec4
SVANNER <stephane.vannerom@gmail.com>
2017-04-06 21:33:17 +0200

    Add files via upload

--- www/CameraPreview.js
+++ www/CameraPreview.js
@@ -4,119 +4,147 @@ var argscheck = require('cordova/argscheck'),
 
 var PLUGIN_NAME = "CameraPreview";
 
-var CameraPreview = function(){};
+var CameraPreview = function() {};
 
-function isFunction(obj){
-  return !!(obj && obj.constructor && obj.call && obj.apply);
+function isFunction(obj) {
+    return !!(obj && obj.constructor && obj.call && obj.apply);
 };
 
-CameraPreview.startCamera = function(options, onSuccess, onError){
-  options = options || {};
-  options.x = options.x || 0;
-  options.y = options.y || 0;
-  options.width = options.width || window.screen.width;
-  options.height = options.height || window.screen.height;
-  options.camera = options.camera || CameraPreview.CAMERA_DIRECTION.FRONT;
-  if(typeof(options.tapPhoto) === 'undefined'){
-    options.tapPhoto = true;
-  }
-  options.previewDrag = options.previewDrag || false;
-  options.toBack = options.toBack || false;
-  if(typeof(options.alpha) === 'undefined'){
-    options.alpha = 1;
-  }
+CameraPreview.startCamera = function(options, onSuccess, onError) {
+    options = options || {};
+    options.x = options.x || 0;
+    options.y = options.y || 0;
+    options.width = options.width || window.screen.width;
+    options.height = options.height || window.screen.height;
+    options.camera = options.camera || CameraPreview.CAMERA_DIRECTION.FRONT;
+    if (typeof(options.tapPhoto) === 'undefined') {
+        options.tapPhoto = true;
+    }
+    options.previewDrag = options.previewDrag || false;
+    options.toBack = options.toBack || false;
+    if (typeof(options.alpha) === 'undefined') {
+        options.alpha = 1;
+    }
 
-  exec(onSuccess, onError, PLUGIN_NAME, "startCamera", [options.x, options.y, options.width, options.height, options.camera, options.tapPhoto, options.previewDrag, options.toBack, options.alpha]);
+    exec(onSuccess, onError, PLUGIN_NAME, "startCamera", [options.x, options.y, options.width, options.height, options.camera, options.tapPhoto, options.previewDrag, options.toBack, options.alpha]);
 };
 
-CameraPreview.stopCamera = function(onSuccess, onError){
-  exec(onSuccess, onError, PLUGIN_NAME, "stopCamera", []);
+CameraPreview.stopCamera = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "stopCamera", []);
 };
 
-CameraPreview.switchCamera = function(onSuccess, onError){
-  exec(onSuccess, onError, PLUGIN_NAME, "switchCamera", []);
+CameraPreview.switchCamera = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "switchCamera", []);
 };
 
-CameraPreview.hide = function(onSuccess, onError){
-  exec(onSuccess, onError, PLUGIN_NAME, "hideCamera", []);
+CameraPreview.hide = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "hideCamera", []);
 };
 
-CameraPreview.show = function(onSuccess, onError){
-  exec(onSuccess, onError, PLUGIN_NAME, "showCamera", []);
+CameraPreview.show = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "showCamera", []);
 };
 
-CameraPreview.takePicture = function(opts, onSuccess, onError){
-  if(!opts){
-    opts = {};
-  }else if(isFunction(opts)){
-    onSuccess = opts;
-    opts = {};
-  }
+CameraPreview.takePicture = function(opts, onSuccess, onError) {
+    if (!opts) {
+        opts = {};
+    } else if (isFunction(opts)) {
+        onSuccess = opts;
+        opts = {};
+    }
 
-  if(!isFunction(onSuccess)){
-    return false;
-  }
+    if (!isFunction(onSuccess)) {
+        return false;
+    }
 
-  opts.width = opts.width || 0;
-  opts.height = opts.height || 0;
+    opts.width = opts.width || 0;
+    opts.height = opts.height || 0;
 
-  if(!opts.quality || opts.quality > 100 || opts.quality < 0){
-    opts.quality = 85;
-  }
+    if (!opts.quality || opts.quality > 100 || opts.quality < 0) {
+        opts.quality = 85;
+    }
 
-  exec(onSuccess, onError, PLUGIN_NAME, "takePicture", [opts.width, opts.height, opts.quality]);
+    exec(onSuccess, onError, PLUGIN_NAME, "takePicture", [opts.width, opts.height, opts.quality]);
 };
 
-CameraPreview.setColorEffect = function(effect, onSuccess, onError){
-  exec(onSuccess, onError, PLUGIN_NAME, "setColorEffect", [effect]);
+CameraPreview.setColorEffect = function(effect, onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "setColorEffect", [effect]);
 };
 
-CameraPreview.setZoom = function(zoom, onSuccess, onError){
-  exec(onSuccess, onError, PLUGIN_NAME, "setZoom", [zoom]);
+CameraPreview.setZoom = function(zoom, onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "setZoom", [zoom]);
 };
 
-CameraPreview.setPreviewSize = function(dimensions, onSuccess, onError){
-  dimensions = dimensions || {};
-  dimensions.width = dimensions.width || window.screen.width;
-  dimensions.height = dimensions.height || window.screen.height;
-
-  return exec(onSuccess, onError, PLUGIN_NAME, "setPreviewSize", [dimensions.width, dimensions.height]);
+CameraPreview.getMaxZoom = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "getMaxZoom", []);
 };
 
-CameraPreview.getSupportedPictureSizes = function(onSuccess, onError){
-  exec(onSuccess, onError, PLUGIN_NAME, "getSupportedPictureSizes", []);
+CameraPreview.getZoom = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "getZoom", []);
+};
+
+CameraPreview.setPreviewSize = function(dimensions, onSuccess, onError) {
+    dimensions = dimensions || {};
+    dimensions.width = dimensions.width || window.screen.width;
+    dimensions.height = dimensions.height || window.screen.height;
+
+    return exec(onSuccess, onError, PLUGIN_NAME, "setPreviewSize", [dimensions.width, dimensions.height]);
+};
+
+CameraPreview.getSupportedPictureSizes = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "getSupportedPictureSizes", []);
 };
 
 CameraPreview.setFlashMode = function(flashMode, onSuccess, onError) {
-  exec(onSuccess, onError, PLUGIN_NAME, "setFlashMode", [flashMode]);
+    exec(onSuccess, onError, PLUGIN_NAME, "setFlashMode", [flashMode]);
 };
 
-CameraPreview.tapToFocus = function(xPoint, yPoint, onSuccess, onError){
-  exec(onSuccess, onError, PLUGIN_NAME, "tapToFocus", [xPoint, yPoint]);
+CameraPreview.tapToFocus = function(xPoint, yPoint, onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "tapToFocus", [xPoint, yPoint]);
+};
+
+CameraPreview.getAutoExposureLock = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "getAutoExposureLock", []);
+};
+
+CameraPreview.setAutoExposureLock = function(lock, onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "setAutoExposureLock", [lock]);
+};
+
+CameraPreview.getExposureCompensation = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "getExposureCompensation", []);
+};
+
+CameraPreview.setExposureCompensation = function(exposureCompensation, onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "setExposureCompensation", [exposureCompensation]);
+};
+
+CameraPreview.getExposureCompensationRange = function(onSuccess, onError) {
+    exec(onSuccess, onError, PLUGIN_NAME, "getExposureCompensationRange", []);
 };
 
 CameraPreview.FLASH_MODE = {
-  OFF: 'off',
-  ON: 'on',
-  AUTO: 'auto',
-  TORCH: 'torch' // Android Only
+    OFF: 'off',
+    ON: 'on',
+    AUTO: 'auto',
+    TORCH: 'torch' // Android Only
 };
 
 CameraPreview.COLOR_EFFECT = {
-  AQUA: 'aqua', // Android Only
-  BLACKBOARD: 'blackboard', // Android Only
-  MONO: 'mono',
-  NEGATIVE: 'negative',
-  NONE: 'none',
-  POSTERIZE: 'posterize',
-  SEPIA: 'sepia',
-  SOLARIZE: 'solarize', // Android Only
-  WHITEBOARD: 'whiteboard' // Android Only
+    AQUA: 'aqua', // Android Only
+    BLACKBOARD: 'blackboard', // Android Only
+    MONO: 'mono',
+    NEGATIVE: 'negative',
+    NONE: 'none',
+    POSTERIZE: 'posterize',
+    SEPIA: 'sepia',
+    SOLARIZE: 'solarize', // Android Only
+    WHITEBOARD: 'whiteboard' // Android Only
 };
 
 CameraPreview.CAMERA_DIRECTION = {
-  BACK: 'back',
-  FRONT: 'front'
+    BACK: 'back',
+    FRONT: 'front'
 };
 
 module.exports = CameraPreview;
