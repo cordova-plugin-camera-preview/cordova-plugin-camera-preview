aef3430daff440b4b8d7bc8606c1027ccdfc5ccd
Weston Ganger <westonganger@gmail.com>
2017-10-02 13:19:25 -0700

    Revert "use older android.media.ExifInterface"
    This reverts commit eabc693367940fe47b9044c0eb3420eca3f64ac2.

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -33,7 +33,7 @@ import android.view.ViewTreeObserver;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.media.ExifInterface;
+import android.support.media.ExifInterface;
 
 import org.apache.cordova.LOG;
 
